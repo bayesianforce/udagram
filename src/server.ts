53902ec43a48a05fd7546b86747b8b0d3a147689
valerio bartolini <valerio.bartolini@ext.addisongloballtd.com>
2020-05-20 23:22:28 +0200

    feat: implementing filteredimage endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 (async () => {
 
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -17,7 +17,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -27,16 +26,30 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
+  app.get( "/filteredimage", async ( req: Request, res: Response ) => {
+    const { image_url } = req.query;
+
+    if ( !image_url ) {
+      res.sendStatus(404).send("Image Url not found");
+    }
+
+    try {
+    const imagePath: string = await filterImageFromURL(image_url);
+
+    res.sendStatus(200).sendFile(imagePath, () => deleteLocalFiles([imagePath]));
+    } catch {
+    res.sendStatus(500).send("Invalid Image Path");
+    }
+  });
   /**************************************************************************** */
 
-  //! END @TODO1
-  
+  // ! END @TODO1
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+    res.send("try GET /filteredimage?image_url={{}}");
   } );
-  
 
   // Start the Server
   app.listen( port, () => {
